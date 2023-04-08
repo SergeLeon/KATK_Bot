86456d69912025a72831bad8b3f0760881841326
Sergey Kravchenko <g605413@gmail.com>
2023-04-08 08:53:02 +0300

    feat: add script for db analysis

---
+++ utils/db_analysis.py
@@ -0,0 +1,25 @@
+import sys
+from collections import Counter
+from pathlib import Path
+
+# Для возможности вызова напрямую
+sys.path.append(str(Path(__file__).parent.parent.absolute()))
+
+from database import DataBase
+
+
+def main():
+    db = DataBase("../database.db")
+
+    users = db.get_all()
+    groups = []
+    for i in users:
+        print(i)
+        groups.extend(i.groups)
+
+    c = Counter(groups)
+    print(len(users), len(c), c)
+
+
+if __name__ == "__main__":
+    main()
